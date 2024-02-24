13c27a76f483da1abb5d0eec7fa22bb0e3586efb
Shihab Ahmed <shihabwp@yahoo.com>
2024-02-23 20:58:15 -0800

    1.0.2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cyberindav/xplore',
+        'pretty_version' => '1.0.2',
+        'version' => '1.0.2.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cyberindav/xplore' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
